defa66f89479b334902d84bd8709ee5db41e0b99
Broc Nickodemus <Broc Nickodemus>
2016-04-12 00:29:31 -0700

    added files

---
+++ projectile motion/Projectile Motion.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button1.Location = New System.Drawing.Point(22, 74)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 51)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Determine Maximun Height:"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(22, 144)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(207, 46)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Determine Approximate Time until Ball Hits the Ground"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(246, 74)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 51)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Display Table"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(246, 144)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(85, 46)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Quit"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Initial Height:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(192, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Initial Velocity:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(116, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(59, 22)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(295, 26)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(58, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(54, 212)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(260, 164)
+        Me.ListBox1.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 40)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "(feet)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(192, 43)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 17)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "(feet / second)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(382, 400)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Projectile Motion"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ projectile motion/Projectile Motion.vb
@@ -0,0 +1,129 @@
+﻿
+
+Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim maxheight, initialHeight, timeToMaxHeight, v As Double
+
+        initialHeight = CDbl(TextBox1.Text)
+
+        v = CDbl(TextBox2.Text)
+
+        timeToMaxHeight = v / 32
+
+        maxheight = initialHeight + v * (timeToMaxHeight) - 16 * (timeToMaxHeight) ^ 2
+
+        MsgBox(maxheight & " feet")
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim v As Double = CDbl(TextBox2.Text)
+
+        Dim initialheight As Double = CDbl(TextBox1.Text)
+
+        Dim t, height As Double
+
+        Dim hitsGround As Boolean = False
+
+        CalculateTime(v, initialheight, t, height, hitsGround)
+
+    End Sub
+
+
+
+    Private Function CalculateTime(v As Double, initialHeight As Double, t As Double, Height As Double, hitsground As Boolean) As Double
+
+        Do Until hitsground
+
+            Height = CInt(initialHeight + v * t - 16 * t ^ 2)
+
+            If Height <= 1 Then
+
+                hitsground = True
+
+            End If
+
+            t += 0.1
+
+        Loop
+
+        Return MsgBox(t & " seconds")
+
+    End Function
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim height As Double = CDbl(TextBox1.Text)
+
+        ListBox1.Items.Clear()
+
+        ListBox1.Items.Add("           Time" & "           Height")
+
+        CalculateChart()
+
+    End Sub
+
+
+
+    Private Sub CalculateChart()
+
+        Dim initialHeight As Double = CDbl(TextBox1.Text)
+
+        Dim v As Double = CDbl(TextBox2.Text)
+
+        Dim height, t As Double
+
+        t = 0.0
+
+        height = 0
+
+        Dim hitsGround As Boolean = False
+
+        WhenHitsGround(initialHeight, v, height, t, hitsGround)
+
+    End Sub
+
+
+
+    Private Sub WhenHitsGround(initialheight As Double, v As Double, height As Double, t As Double, hitsground As Boolean)
+
+        Do Until hitsground
+
+            Dim Str = height
+
+            height = initialheight + v * t - 16 * t ^ 2
+
+            ListBox1.Items.Add("            " & t.ToString("N") & "            " & height.ToString("N1"))
+
+            t = CDec(t + 0.25)
+
+            If height <= 1 Then
+
+                hitsground = True
+
+            End If
+
+        Loop
+
+    End Sub
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Close()
+
+    End Sub
+
+
+
+End Class
